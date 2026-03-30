0616edd2b4a4c852e55b1b6f4851f2d47c7664c5
Mona Lisa <gabrielhenrique.pessoal@gmail.com>
2026-03-30 17:16:47 -0300

    ajustes no hero para mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,25 +7,24 @@ import { H1 } from './ui/H1';
 export function Hero() {
   return (
     <section
-      className="bg-orange flex flex-col items-center px-12 py-12 w-full h-[75dvh]"
+      className="bg-orange flex flex-col items-center px-4 md:px-12 py-8 md:py-12 w-full h-[60dvh] md:h-[75dvh]"
       data-name="hero"
-      data-node-id="10:4"
     >
       <div className="overflow-hidden rounded-xl relative w-full h-full" data-name="heroImage">
         <Image
           src={heroImage}
-          alt="Casal"
+          alt="Por do sol"
           fill
           className="object-cover"
           priority
         />
 
         {/* Texto sobreposto */}
-        <div className="absolute inset-0 flex items-center justify-between px-12 z-10">
-          <H1 className="max-w-[465px] uppercase text-cream">
+        <div className="absolute inset-0 flex flex-col md:flex-row items-left md:items-center justify-center justify-between gap-4 md:gap-4 px-4 py-8 md:px-12 z-10">
+          <H1 className="w-full max-w-[465px] uppercase text-cream text-left text-2xl md:text-4xl">
             Celebramos o agora com tudo o que já fomos
           </H1>
-          <H1 className="max-w-[272px] uppercase text-cream">
+          <H1 className="w-full max-w-[272px] uppercase text-cream text-left text-2xl md:text-4xl">
             e tudo o que ainda seremos
           </H1>
         </div>
